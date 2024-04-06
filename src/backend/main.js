5b469b1c600a3f0cc1ddffbc65d64d6f12f25430
yusufgulermi <my_name0724@hotmail.com>
2024-04-06 11:25:16 +0300

    search functionality added & for favorite button pinia added

--- src/backend/main.js
+++ src/backend/main.js
@@ -1,11 +1,16 @@
 import '@/frontend/assets/main.css'
 import { createApp } from 'vue'
+import { createPinia  } from 'pinia';
 
 import App from '@/frontend/App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
+
+app.use(pinia)
 app.use(router)
+
 app.mount('#app')
 
 
